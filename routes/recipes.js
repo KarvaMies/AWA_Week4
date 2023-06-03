b55a6c2284ccd6885494a9ee3889790cd2c8fce4
KarvaMies <jeremias.moberg@gmail.com>
2023-06-03 16:34:07 +0300

    updated task 2

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,51 +1,49 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
 
 router.get('/', function(req, res, next) {
   res.send("You forgot the recipe from the URL.");
 });
 
 router.get("/:food", function(req, res) {
-  console.log(`recipe name: ${req.params.food}`);
   recipe = {
     name: req.params.food,
-    instructions: "First do this. After that do that.",
+    instructions: ["First do this.", "After this do that."],
     ingredients: ["ingredient#1", "ingredient#2", "ingredient#3", "ingredient#4", "ingredient#5"]
   };
 
-  /*
-  let recipeName = document.createElement("h2");
-  recipeName.innerText = recipe.name;
-
-  let instructions = document.createElement("p");
-  instructions.innerText = recipe.instructions;
-  
-  let ingredientsList = document.createElement("ul");
-
-  recipe.ingredients.forEach(function(ingredient) {
-    let li = document.createElement("li");
-    li.innerText = ingredient;
-    ingredientsList.appendChild(li);
-  });
-
-  let container = document.getElementById("recipe-container");
-  container.appendChild(recipeName);
-  container.appendChild(instructions);
-  container.appendChild(ingredientsList);
-  */
-
   let html = `
     <h2>${recipe.name}</h2>
-    <p>${recipe.instructions}</p>
+    <h3>Instructions:</h3>
+    <ul>
+    ${recipe.instructions.map(instruction => `<li>${instruction}</li>`).join('')}
+    </ul>
+    <h3>Ingredients:</h3>
     <ul>
       ${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
     </ul>
   `;
 
+  let htmlPath = path.join(__dirname, '../public/index.html');
+  fs.readFile(htmlPath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading recipes.html file');
+      return;
+    }
+
+    updatedHtml = data.replace('<div id="recipe-container"></div>', `<div id="recipe-container">${html}</div>`);
+
+    res.send(updatedHtml);
+  })
+
+
+
   //res.send(html);
   //res.sendFile(path.join(__dirname, '../public/recipes.html'));
-  res.json(recipe);
+  //res.json(recipe);
 })
 
 
